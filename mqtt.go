969d4baded0d18640610f499633bfc47d4bcb34b
Tim Becker <tim@presseverykey.com>
2021-05-27 10:06:23 +0200

    better recommit functionality

--- mqtt.go
+++ mqtt.go
@@ -1,7 +1,10 @@
 package mqttGather
 
 import (
+	"crypto/tls"
 	"log"
+	"net/url"
+	"time"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
@@ -52,20 +55,38 @@ func NewMQTT(cfg *RunConfig, db DB) (*Mqtt, error) {
 	opts.AddBroker(mqtt.Broker)
 	opts.SetClientID(mqtt.ClientId)
 
+	opts.SetConnectRetryInterval(10 * time.Second)
+	opts.SetConnectionAttemptHandler(func(u *url.URL, cfg *tls.Config) *tls.Config {
+		log.Printf("D: connection attempt: %v", u)
+		return cfg // why!?
+	})
+	opts.SetConnectionLostHandler(func(c MQTT.Client, err error) {
+		log.Printf("E: connection lost: %v", err)
+	})
 	opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
 		log.Printf("I: unexpected message on topic: %s : %s", msg.Topic(), string(msg.Payload()))
 		// TODO log to db
 	})
+	opts.SetOnConnectHandler(func(c MQTT.Client) {
+		opts := c.OptionsReader()
+		log.Printf("D: connect: %v", opts.ClientID())
+		token := mqtt.client.Subscribe(mqtt.Topic, byte(0), mqtt.msgHandler)
+		if token.Wait() && token.Error() != nil {
+			log.Printf("E: subscribtion failed: %s (%v)", mqtt.Topic, token.Error())
+			// TODO figure out what to do here: sit under a tree crying and waiting to die?
+		} else {
+			log.Printf("D: subscribed to: %s", mqtt.Topic)
+		}
+	})
+	opts.SetReconnectingHandler(func(c MQTT.Client, o *MQTT.ClientOptions) {
+		opts := c.OptionsReader()
+		time.Sleep(2 * time.Second) // don't just hammer away at poor server.
+		log.Printf("I: reconnecting: %v", opts.ClientID())
+	})
 
 	mqtt.client = MQTT.NewClient(opts)
+
 	token := mqtt.client.Connect()
-
-	if token.Wait() && token.Error() != nil {
-		return nil, token.Error()
-	}
-
-	token = mqtt.client.Subscribe(mqtt.Topic, byte(0), mqtt.msgHandler)
-
 	if token.Wait() && token.Error() != nil {
 		return nil, token.Error()
 	}
